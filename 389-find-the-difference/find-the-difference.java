a61ca9a195f8f615b5df7b48337c567e7ed88826
joshih121-m <joshi.h1121@gmail.com>
2026-08-07 15:59:25 +0530

    Time: 3 ms (41.59%) | Memory: 43.4 MB (20.23%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,16 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char []a =s.toCharArray();
+        char []b =t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for(int i =0;i<s.length();i++)
+        {
+            if(a[i]!= b[i])
+            {
+                return b[i];
+            }
+        }
+        return b[b.length-1];
+    }
+}
