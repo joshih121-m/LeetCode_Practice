6d867981ffa1e4f7aeca401096474759e997cc0b
joshih121-m <joshi.h1121@gmail.com>
2026-08-12 11:57:48 +0530

    Time: 4 ms (1.35%) | Memory: 42.8 MB (61.51%) - LeetSync

---
+++ 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkString(String s) {
+        char arr[]= s.toCharArray();
+        for(int i=0;i<s.length();i++)
+        {
+            for( int j=i+1;j<s.length();j++)
+            {
+                if(s.charAt(i)=='b' && s.charAt(i+1)=='a'){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
