0ab19bbb338d3e27c8ef26e24a07ad232e08a756
joshih121-m <joshi.h1121@gmail.com>
2026-08-20 10:49:01 +0530

    Time: 2 ms (73.55%) | Memory: 43 MB (11.89%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isSubsequence(String s, String t) {
         int i=0;
         int j=0;
-        while(i<s.length()&&j<t.length())
+        while(i<s.length() && j<t.length())
         {
             
             if(s.charAt(i)==t.charAt(j)){
@@ -18,7 +18,7 @@ class Solution {
             return true;
         }
         else{
-             return false;
+             return  false ;
         }
     }
 }
