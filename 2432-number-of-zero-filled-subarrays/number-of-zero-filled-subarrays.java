ec6fbb45b770df2c1f590af7989631c6e2af14bc
joshih121-m <joshi.h1121@gmail.com>
2026-08-28 17:12:14 +0530

    Time: 3 ms (96.87%) | Memory: 115.5 MB (68.39%) - LeetSync

---
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        int count=0;
+        long ans =0;
+        for(int i=0;i<nums.length;i++)
+        {
+         if(nums[i]==0){
+            count++;
+            ans = ans +count;
+         }
+         else
+         { count=0;
+         }
+        }
+        return ans;
+    }
+}
